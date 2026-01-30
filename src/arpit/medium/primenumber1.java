ef573cfe8c64325249f051f753baa6f325d526ef
arpit-yadav01 <arpityadav170402@gmail.com>
2026-01-31 02:49:11 +0530

    string  palindrome

---
+++ src/arpit/medium/primenumber1.java
@@ -0,0 +1,22 @@
+//print prime number 1 t 100
+
+package src.arpit.medium;
+
+public class primenumber1 {
+    public static void main(String[] args){
+  
+        for(int num=2; num<=100; num++){
+            int count =0;
+        
+        for(int i=1; i<=num ; i++){
+            if(num % i==0){
+                count++;
+            }
+            
+        }
+        if(count ==2)
+            System.out.println(num);
+    }
+    }
+
+}
