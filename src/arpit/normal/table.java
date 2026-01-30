7a97910f7f022813bf87fc0fecb107bc3993fc8b
arpit-yadav01 <arpityadav170402@gmail.com>
2026-01-30 16:21:46 +0530

    first commit

---
+++ src/arpit/normal/table.java
@@ -0,0 +1,14 @@
+package src.arpit.normal;
+
+public class table {
+   
+
+     public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a=2;
+        for(int i=1;i<=10;i++){
+            System.out.println(a+" x "+i+" = "+(a*i));
+        }
+    }
+}
+
