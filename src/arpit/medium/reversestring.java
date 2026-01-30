bb90f7412d4da90cbf562d64a2f31881ecbb0c0c
arpit-yadav01 <arpityadav170402@gmail.com>
2026-01-31 01:42:26 +0530

    reverse string

---
+++ src/arpit/medium/reversestring.java
@@ -0,0 +1,18 @@
+package src.arpit.medium;
+import java.util.Scanner;
+public class reversestring {
+   public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    System.out.println("enter the string");
+     String str = sc.nextLine(); 
+
+    String reverse= " ";
+
+    for(int i=str.length()-1; i>=0 ;i--){
+            reverse= reverse+ str.charAt(i);
+    }
+    System.out.println (reverse + " ");
+
+   }
+    
+}
