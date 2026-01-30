a3e7c79a77b41e368afc2d6b26473e29872c16ac
arpit-yadav01 <arpityadav170402@gmail.com>
2026-01-31 02:21:16 +0530

    string  palindrome

---
+++ src/arpit/medium/stringpalindrome.java
@@ -0,0 +1,25 @@
+package src.arpit.medium;
+import java.util.Scanner;
+
+public class stringpalindrome {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str= sc.nextLine();
+
+        
+        String reverse="";
+
+        for(int i =str.length()-1; i>=0 ; i--){
+            reverse= reverse+ str.charAt(i);
+        }
+
+        if(str.equals(reverse))
+            System.out.println("palindrome");
+        else
+            System.out.println("not a palindrome");
+
+
+    }
+    
+}
