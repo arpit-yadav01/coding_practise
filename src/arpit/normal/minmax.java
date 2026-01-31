9044e832c7165313fbf4ad41bb5deccf2b77b647
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-01 01:36:08 +0530

    new entry

--- src/arpit/normal/minmax.java
+++ src/arpit/normal/minmax.java
@@ -2,6 +2,27 @@ package src.arpit.normal;
 
 public class minmax {
     
-    public static void main
+    public static void main(String[] args){
+        int [] arr={1,2,3,4,5,5,6};
+
+        int min = arr[0];
+        int max= arr[0];
+
+        for(int i = 0 ; i <arr.length; i++){
+            if(arr[i]>max){
+                max= arr[i];
+                // 
+            }
+            if (arr[i]<min){
+                min =arr[i];
+                
+            }
+            
+        
+            
+        }
+        System.out.println("the maximum number is " + max);
+        System.out.println(" the minimum number is  " + min);
+    }
     
 }
