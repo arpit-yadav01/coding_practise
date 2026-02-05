b85ed28e300da71d3c247118a0e3a3e3efcc6608
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-05 15:11:43 +0530

    fib

--- src/arpit/medium/fibonacci.java
+++ src/arpit/medium/fibonacci.java
@@ -3,18 +3,19 @@ package src.arpit.medium;
 public class fibonacci {
     public static void main( String [] args){
 
-        int number =10;
-        int a =0 ;
-        int b=1;
 
-        for(int i =0 ; i<number ; i++){
-            System.out.print(a + " ");
-            int c = a+b;
-            
-            a=b  ;
+        int n =12;
+
+        int a=0; 
+        int b=1; 
+       
+        for(int i=0 ; i<n ; i++){
+            System.out.println( a + " ");
+             int c= a+b;
+            a=b ;
             b=c;
         }
-       
+        
     }
     
 }
