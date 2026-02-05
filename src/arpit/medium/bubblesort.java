5559155ceb502cabdfc7d1b7d453e7b8e758e267
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-05 19:00:31 +0530

    sorting

--- src/arpit/medium/bubblesort.java
+++ src/arpit/medium/bubblesort.java
@@ -1,8 +1,28 @@
 package src.arpit.medium;
 
+import java.util.Arrays;
+
 public class bubblesort {
     public static void main(String[] main){
-        
+        int [] arr ={1,4,3,2,7,6 };
+
+        //  Arrays.sort(arr);
+        //  System.out.println(Arrays.toString(arr));
+
+        for(int i=0; i<arr.length -1; i++){
+            for(int j=0; j<arr.length -1 -i; j++){
+                if(arr[j]> arr[j+1]){
+                    int temp= arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp ;
+                }
+            }
+        }
+        System.out.print("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
     }
     
 }
