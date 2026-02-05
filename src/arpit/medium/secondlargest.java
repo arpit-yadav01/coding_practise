2ccf43dece5ff077fddbea5031edb7b68b5540e3
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-05 15:21:03 +0530

    fib

--- src/arpit/medium/secondlargest.java
+++ src/arpit/medium/secondlargest.java
@@ -2,23 +2,22 @@ package src.arpit.medium;
 
 public class secondlargest {
     public static void main(String[] args){
-        int [] arr={ 1,5,3,7,8};
-int max = Integer.MIN_VALUE;
-int max2 = Integer.MIN_VALUE;
-        for(int i =0 ;i<arr.length;i++)
-        {
-           
-            
-            if(arr[i]>max)
-            {
-                max2 = max;
-                max = arr[i];
-            
-                
-            }
+        int [] arr ={1,3,5,6,8,7};
 
-        }
-        System.out.println("max : "+max+" second max : "+max2);
+        int max = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        for(int i =0; i <arr.length; i++){
+            if (arr[i]> max)
+{
+    max2 = max;
+    max= arr[i];
+}  
+else if (arr[i] > max2  && arr[i] != max) {
+                max2 = arr[i];
+            }
+      }
+System.out.println("the maximum is " +max + ",  the second maximum " + max2) ;
     }
     
 }
