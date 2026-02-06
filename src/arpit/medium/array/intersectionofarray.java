2002e9b63a776598688e7ebcacbddc1dcaa1d700
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-06 16:20:05 +0530

    sliding window

---
+++ src/arpit/medium/array/intersectionofarray.java
@@ -0,0 +1,20 @@
+package src.arpit.medium.array;
+
+public class intersectionofarray {
+    public static void main(String[] args){
+        int [] arr1 = {1,2,3,4};
+        int [] arr2 ={5, 6, 3 ,8};
+       
+
+        for(int i =0; i< arr1.length ; i++){
+            for(int j = 0 ; j <arr2.length ; j++ ){
+                if(arr1[i ]== arr2[j]){
+                    System.out.println ("the intersection point is " +  arr1[i]);
+                    break;
+                }
+                
+            }
+        }
+     }
+    
+}
---
+++ src/arpit/medium/array/leftrotate.java
@@ -0,0 +1,27 @@
+package src.arpit.medium.array;
+
+public class leftrotate {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5};
+        int k = 2;
+        int n = arr.length;
+
+        int[] result = new int[n];
+
+        // copy from k to end
+        for(int i = 0; i < n - k; i++){
+            result[i] = arr[i + k];
+        }
+
+        // copy first k elements to end
+        for(int i = 0; i < k; i++){
+            result[n - k + i] = arr[i];
+        }
+
+        // print result
+        for(int num : result){
+            System.out.print(num + " ");
+        }
+    }
+}
