7e41bea147df165d149f2cf7049cc4edc69d246f
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-08 22:39:42 +0530

    find the no repeated string

--- src/arpit/medium/String/nonreapeted.java
+++ src/arpit/medium/String/nonreapeted.java
@@ -2,24 +2,30 @@ package src.arpit.medium.String;
 
 public class nonreapeted {
     public static void main(String[] args){
-        String str = "arpraiti";
-       
+        String str ="arpit";
+        char result = ' ';
 
-        for(int  i =0; i<str.length(); i++){
-            char ch= str.charAt(i);
-            int count=0;
-        
-        for(int j=0; j<str.length() ; j++){
-            if(str.charAt(j)== ch ){
-                count++;
+        for(int i =0; i <str.length() ; i++){
+            char ch = str.charAt(i);
+            int count =0;
+            for(int j =0 ; j<str.length() ; j++){
+                if(str.charAt(j) == ch){
+                    count++;
+                }
             }
-            }
-            if(count == 1){
-                System.out.println("First non-repeating character: " + ch);
+            if(count ==1){
+                result = ch;
                 break;
-        
-    }
+            }
+
+            
+       
+            
         }
-    }
+         if (result != ' ')
+            System.out.println("First non-repeating character: " + result);
+        else
+            System.out.println("No non-repeating character found");
+    }  
     
 }
