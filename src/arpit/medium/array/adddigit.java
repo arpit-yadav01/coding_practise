da2daadb709d48c36531dc2f62e7abd013e5d85d
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-15 16:36:00 +0530

    leetcode quesiotn add digit

---
+++ src/arpit/medium/array/adddigit.java
@@ -0,0 +1,20 @@
+package src.arpit.medium.array;
+
+public class adddigit {
+    public static void main(String [] args){
+        int num = 38;
+
+        while(num>=10){
+            int sum = 0;
+            while(num>0){
+                sum = sum +num%10;
+                num = num /10;
+
+            }
+            num = sum +num;
+        }
+        System.out.println(num);
+
+    }
+    
+}
