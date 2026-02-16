829269948ed7a9dc232d3445eb28a4819b5998dc
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-16 14:54:15 +0530

    armstrong number

--- src/arpit/medium/array/armstrongnumber.java
+++ src/arpit/medium/array/armstrongnumber.java
@@ -3,19 +3,23 @@ package src.arpit.medium.array;
 public class armstrongnumber {
     public static void main(String[] args){
         int num = 153;
-        int original = num;
+        int original =num;
         int sum=0;
 
-        while (num >0){
-            int digit = num  %10;
-            sum =sum + (digit * digit* digit);
-            num = num /10; 
+        while(num>0){
+            int digit = num%10;
+            sum = sum +(digit *digit* digit);
+            num = num/10;
+
+            
+
         }
-        if(sum== original ){
-            System.out.println ("it is armstrong number");
-        }
-        else
-            System.out.println ("it is not armstrong number");
+        if(sum==original){
+                System.out.println("it is armstrong number");
+
+            }else{
+                System.out.println("it is not a armstrong number");
+            }
 
 
     }
--- src/arpit/medium/array/countdigit.java
+++ src/arpit/medium/array/countdigit.java
@@ -3,15 +3,14 @@ package src.arpit.medium.array;
 public class countdigit {
     public static void main(String[] args){
 
-        int [] arr= { 1, 1, 2,3,4,4,5,4,4};
-        int target=4;
+        int [] arr ={1,2,4,5,5,6,6,5,4,2};
+        int target=5;
         int count=0;
-        for(int i =0; i<arr.length; i++){
-            if(arr[i] == target ){
-                count ++;
-            }
+        for (int i =0; i<arr.length; i++){
+            if(arr[i]== target)
+                count++;
         }
-        System.out.println( "target element count is " + count );
+        System.out.println("the number of count is : " +count);
     }
     
 }
