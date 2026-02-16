f7d900974b1f8f322655f0543876a61f70d26a88
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-16 19:56:27 +0530

    remove duplicates

--- src/arpit/medium/Arraylist/removeduplicates.java
+++ src/arpit/medium/Arraylist/removeduplicates.java
@@ -1,5 +1,29 @@
 package src.arpit.medium.Arraylist;
 
+import java.util.ArrayList;
+
 public class removeduplicates {
-    
+     public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(3);
+        list.add(5);
+
+        ArrayList<Integer> unique= new ArrayList<>();
+
+        for(int num : list){
+            if(!unique.contains(num)){
+                unique.add(num);
+            }
+        }
+
+        System.out.println(list);
+        System.out.println(unique);
+    }
 }
