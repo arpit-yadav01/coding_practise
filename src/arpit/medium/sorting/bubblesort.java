000419989cf1a7c684db68d9fe123cb3b8735ddb
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-21 20:03:23 +0530

    bubble sort

--- src/arpit/medium/sorting/bubblesort.java
+++ src/arpit/medium/sorting/bubblesort.java
@@ -4,24 +4,22 @@ import java.util.Arrays;
 
 public class bubblesort {
     public static void main(String[] main){
-        int [] arr ={1,4,3,2,7,6 };
+        int [] arr ={ 2,4,1,3,4,2,7,6,5};
 
-        //  Arrays.sort(arr);
-        //  System.out.println(Arrays.toString(arr));
-
-        for(int i=0; i<arr.length -1; i++){
-            for(int j=0; j<arr.length -1 -i; j++){
-                if(arr[j]> arr[j+1]){
-                    int temp= arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp ;
+        for(int i =0 ; i<= arr.length-1; i++){
+            for(int j =0 ; j <arr.length; j++){
+                if(arr[i ]< arr[j]){
+                    int temp = arr[i];
+                    arr[i]= arr[j];
+                    arr[j]= temp;
                 }
             }
+
+            
         }
-        System.out.print("Sorted array: ");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
+        for (int i =0; i <arr.length; i++){
+                System.out.println(arr[i]+ " ");
+            }
 
     }
     
