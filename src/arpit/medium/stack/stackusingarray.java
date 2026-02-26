09a4bac681973633a14703f86453c9255ce9da33
arpit-yadav01 <arpityadav170402@gmail.com>
2026-02-26 21:08:51 +0530

    stack with array

--- src/arpit/medium/stack/stackusingarray.java
+++ src/arpit/medium/stack/stackusingarray.java
@@ -1,12 +1,13 @@
 package src.arpit.medium.stack;
 
 public class stackusingarray {
-   int [] stack = new int[5];
 
-   static int top=-1;
+    int[] stack = new int[5];
+    int top = -1;
 
-    static void push(int value) {
-        if (top == 4) {
+    // push
+    void push(int value) {
+        if (top == stack.length - 1) {
             System.out.println("Stack Overflow");
         } else {
             top++;
@@ -14,5 +15,47 @@ public class stackusingarray {
             System.out.println(value + " pushed");
         }
     }
-    
+
+    // pop
+    void pop() {
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+        } else {
+            System.out.println(stack[top] + " popped");
+            top--;
+        }
+    }
+
+    // peek
+    void peek() {
+        if (top == -1) {
+            System.out.println("Stack is empty");
+        } else {
+            System.out.println("Top: " + stack[top]);
+        }
+    }
+
+    // display
+    void display() {
+        if (top == -1) {
+            System.out.println("Stack empty");
+        } else {
+            for (int i = top; i >= 0; i--) {
+                System.out.println(stack[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        stackusingarray s = new stackusingarray();
+
+        s.push(10);
+        s.push(20);
+        s.push(30);
+
+        s.display();
+        s.peek();
+        s.pop();
+        s.display();
+    }
 }
