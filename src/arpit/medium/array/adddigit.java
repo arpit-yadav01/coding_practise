5827459da70940b2efa5ab744664ced26412d0b3
arpit-yadav01 <arpityadav170402@gmail.com>
2026-03-02 22:26:16 +0530

    revision

--- src/arpit/medium/array/adddigit.java
+++ src/arpit/medium/array/adddigit.java
@@ -1,20 +1,56 @@
 package src.arpit.medium.array;
 
 public class adddigit {
-    public static void main(String [] args){
-        int num = 38;
+    // public static void main(String [] args){
+    //     int num = 38;
 
-        while(num>=10){
-            int sum = 0;
+    //     while(num>=10){
+    //         int sum = 0;
+    //         while(num>0){
+    //             sum = sum +num%10;
+    //             num = num /10;
+
+    //         }
+    //         num = sum +num;
+    //     }
+    //     System.out.println(num);
+
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args){
+        int num =89;
+
+        
+        while(num >=10){
+            int sum =0;
             while(num>0){
-                sum = sum +num%10;
-                num = num /10;
 
+            sum = sum +num %10;
+            num= num/10;
             }
-            num = sum +num;
+            num = sum +num ;
         }
         System.out.println(num);
-
     }
+    
     
 }
--- src/arpit/medium/array/armstrongnumber.java
+++ src/arpit/medium/array/armstrongnumber.java
@@ -1,27 +1,52 @@
 package src.arpit.medium.array;
 
 public class armstrongnumber {
-    public static void main(String[] args){
-        int num = 153;
-        int original =num;
-        int sum=0;
+    // public static void main(String[] args){
+    //     int num = 153;
+    //     int original =num;
+    //     int sum=0;
 
-        while(num>0){
-            int digit = num%10;
-            sum = sum +(digit *digit* digit);
-            num = num/10;
+    //     while(num>0){
+    //         int digit = num%10;
+    //         sum = sum +(digit *digit* digit);
+    //         num = num/10;
 
             
 
+    //     }
+    //     if(sum==original){
+    //             System.out.println("it is armstrong number");
+
+    //         }else{
+    //             System.out.println("it is not a armstrong number");
+    //         }
+
+
+    // }
+
+
+
+
+
+
+    public static void main(String [] args){
+        int num= 153;
+        int original= num;
+        int sum= 0;
+
+        while(num >0){
+            int digit = num%10;
+            sum  = sum +(digit *digit * digit);
+            num = num/10;
+
+
         }
-        if(sum==original){
-                System.out.println("it is armstrong number");
-
-            }else{
-                System.out.println("it is not a armstrong number");
-            }
-
-
+        if (sum == original ){
+            System.out.println("it is armstrong nunmber");
+        }
+        else{
+            System.out.println("it is not armstrong number");
+        }
     }
     
 }
